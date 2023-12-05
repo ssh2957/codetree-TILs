23453393bf7be3d9f77171e0ebce48d65cc94a36
신승호 <128335473+ssh2957@users.noreply.github.com>
2023-12-05 17:46:59 +0900

    Title: 달에서 무게 구하기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231205/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 0.165;
+        int b = 13;
+        System.out.printf("%d * %.6f = %.6f",b, a , b*a);
+    }
+}
