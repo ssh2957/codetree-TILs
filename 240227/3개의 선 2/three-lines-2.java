d7c0f329373b88b5fba7a54a037350299e134ecb
신승호 <128335473+ssh2957@users.noreply.github.com>
2024-02-27 22:57:38 +0900

    Title: 3개의 선 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240227/3개의 선 2/three-lines-2.java
+++ 240227/3개의 선 2/three-lines-2.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -6,53 +7,154 @@ public class Main {
 		boolean[] sel;
 		int n = sc.nextInt();
 		int[][] arr = new int[n][2];
-		int[] count = new int[11];// 어떤숫자가 있나 확인할 count 배열
-		ArrayList<Integer> arr1 = new ArrayList<>();
-
+		int[] countx = new int[11];// 어떤숫자가 있나 확인할 count 배열
+		int[] county = new int[11];
+		ArrayList<Integer> arrx = new ArrayList<>();
+		ArrayList<Integer> arry = new ArrayList<>();
 		// 배열 가져오기
 		for (int i = 0; i < n; i++) {
 			arr[i][0] = sc.nextInt();
 			arr[i][1] = sc.nextInt();
 
-			count[arr[i][0]]++;
-			count[arr[i][1]]++;
+			countx[arr[i][0]]++;
+			county[arr[i][1]]++;
 
 		}
 
 		for (int i = 0; i < 11; i++) {
-			if (count[i] > 0) {
-				arr1.add(i);
+			if (countx[i] > 0) {
+				arrx.add(i);
 
 			}
 		}
+		
+		for (int i = 0; i < 11; i++) {
+			if (county[i] > 0) {
+				arry.add(i);
 
-		sel = new boolean[n];
+			}
+		}
+		
+		int s= arrx.size()+arry.size();
+
+		
 
 		boolean ans = false;
-		hi: for (int i = 0; i < arr1.size() - 2; i++) {
-			for (int j = i; j < arr1.size() - 1; j++) {
-				for (int k = j; k < arr1.size(); k++) {
+		hi: for (int i = 0; i < s - 2; i++) {
+			for (int j = i; j < s - 1; j++) {
+				for (int k = j; k < s; k++) {
 
+					sel = new boolean[n];
 					int cnt = 0;
-					for (int l = 0; l < n; l++) {
-						if (arr[l][0] == i || arr[l][0] == j || arr[l][0] == k || arr[l][1] == i || arr[l][1] == j
-								|| arr[l][1] == k) {
-							sel[l] = true;
+					// 모두 세로줄
+					if(i>=arrx.size()) {
+						int a =i-arrx.size();
+						int b =j-arrx.size();
+						int c =k-arrx.size();
+						for (int l = 0; l < n; l++) {
+							if (arr[l][1] == arry.get(a) || arr[l][1] == arry.get(b)
+									|| arr[l][1] == arry.get(c)) {
+								sel[l] = true;
+
+							}
 
 						}
+						for (int l = 0; l < n; l++) {
+							if (sel[l]) {
+								cnt++;
+							}
 
-					}
-
-					for (int l = 0; l < n; l++) {
-						if (sel[l]) {
-							cnt++;
 						}
+						if (cnt == n) {
+							ans = true;
+							break hi;
+						}
+						
+						
+						
+						
+						
+						
+						
+					}
+					//j,k만 세로줄
+					else if(j>=arrx.size()) {
+						int a =i;
+						int b =j-arrx.size();
+						int c =k-arrx.size();
+						
+						for (int l = 0; l < n; l++) {
+							if (arr[l][0] == arrx.get(a) || arr[l][1] == arry.get(b)
+									|| arr[l][1] == arry.get(c)) {
+								sel[l] = true;
 
+							}
+
+						}
+						for (int l = 0; l < n; l++) {
+							if (sel[l]) {
+								cnt++;
+							}
+
+						}
+						if (cnt == n) {
+							ans = true;
+							break hi;
+						}
+						
 					}
-					if (cnt == n) {
-						ans = true;
-						break hi;
+					//k만 세로줄
+					else if(k>=arrx.size()) {
+						int a =i;
+						int b =j;
+						int c =k-arrx.size();
+						for (int l = 0; l < n; l++) {
+							if (arr[l][0] == arrx.get(a) || arr[l][0] == arrx.get(b)
+									|| arr[l][1] == arry.get(c)) {
+								sel[l] = true;
+
+							}
+
+						}
+						for (int l = 0; l < n; l++) {
+							if (sel[l]) {
+								cnt++;
+							}
+
+						}
+						if (cnt == n) {
+							ans = true;
+							break hi;
+						}
+						
 					}
+					// 다 가로줄
+					else {
+						int a =i;
+						int b =j;
+						int c =k;
+						for (int l = 0; l < n; l++) {
+							if (arr[l][0] == arrx.get(a) || arr[l][0] == arrx.get(b)
+									|| arr[l][0] == arrx.get(c)) {
+								sel[l] = true;
+
+							}
+
+						}
+						for (int l = 0; l < n; l++) {
+							if (sel[l]) {
+								cnt++;
+							}
+
+						}
+						if (cnt == n) {
+							ans = true;
+							break hi;
+						}
+						
+						
+					}
+					
 
 				}
 			}
